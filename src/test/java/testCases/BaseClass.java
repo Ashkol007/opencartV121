e53ca4dd954e233eaadb6bc56d48639a992a5179
Ashitosh Kole <ashitoshkole007@gmail.com>
2025-09-01 17:52:57 +0530

    updated successfully 1-09-2025

--- src/test/java/testCases/BaseClass.java
+++ src/test/java/testCases/BaseClass.java
@@ -17,6 +17,7 @@ import org.openqa.selenium.TakesScreenshot;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.edge.EdgeDriver;
+import org.openqa.selenium.firefox.FirefoxDriver;
 import org.testng.annotations.AfterClass;
 import org.testng.annotations.AfterMethod;
 import org.testng.annotations.BeforeClass;
@@ -25,6 +26,7 @@ import org.testng.annotations.Parameters;
 
 public class BaseClass {
 	
+	public String br;
 	public static WebDriver driver;
     public static Logger logger; 
     public Properties p;
@@ -41,6 +43,7 @@ public class BaseClass {
 		
 		case "chrome": driver = new ChromeDriver(); break;
 		case "edge" : driver = new EdgeDriver(); break;
+        case "firefox" : driver = new FirefoxDriver(); break;
 		default : System.out.println("INvalid Browser !!");
 		
 		}
--- src/test/java/testCases/TC0018_ToolTipValidationMessageForEmail.java
+++ src/test/java/testCases/TC0018_ToolTipValidationMessageForEmail.java
@@ -7,6 +7,7 @@ import org.openqa.selenium.JavascriptExecutor;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.remote.RemoteWebDriver;
 import org.openqa.selenium.support.ui.ExpectedConditions;
 import org.openqa.selenium.support.ui.WebDriverWait;
 import org.testng.Assert;
@@ -18,8 +19,13 @@ import utilities.EmailVerification;
 
 public class TC0018_ToolTipValidationMessageForEmail extends BaseClass {
 	
+
 	@Test(groups={"Master"})
 	public void emailVerfication() {
+		
+
+	    String browserName = ((RemoteWebDriver) driver).getCapabilities().getBrowserName();
+		
 		
 try {
 			
@@ -31,10 +37,22 @@ try {
 		
 		    AccountRegistrationPage Arp = new AccountRegistrationPage(driver);
 		                            Arp.setEmail("ashi123");
-		                            Arp.clickContinue();
-		      
-		   logger.info("$$EmailToolTipWarningMsg$$ :"+Arp.getEmailToolTipWarningMsg());
-		    Assert.assertTrue(Arp.getEmailToolTipWarningMsg().contains("Please include an '@' in the email address."));
+		                       Arp.clickContinue();
+
+		    if(browserName.equalsIgnoreCase("firefox")) {
+		    	
+
+				   logger.info("$$FirfoxEmailToolTipWarningMsg$$ :"+Arp.getEmailToolTipWarningMsg());
+				    Assert.assertTrue(Arp.getEmailToolTipWarningMsg().trim().contains("Please enter an email address."));
+				    
+		    }else {
+		    	
+
+				   logger.info("$$EmailToolTipWarningMsg$$ :"+Arp.getEmailToolTipWarningMsg());
+				    Assert.assertTrue(Arp.getEmailToolTipWarningMsg().contains("Please include an '@' in the email address."));
+		    }                   
+		  
+		                            
 		    
 		    
 		    logger.info(" ************** TC0018_Email Validation TestCase ended *********** ");
